d45470a8a8c4aeb35040185471d44250f72fa0bc
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-13 13:25:02 +0500

    added, refactor!: added widgetes/header, refactor eslintrc.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Route, Routes } from 'react-router';
+import { Header } from 'widgets/header';
 
 const Basket = React.lazy(() => import('./basket'));
 const FoodDetail = React.lazy(() => import('./food-detail'));
@@ -8,13 +9,16 @@ const MenuOfFoof = React.lazy(() => import('./food-menu'));
 const Main = React.lazy(() => import('./main'));
 
 const Pages: FC = () => (
-  <Routes>
-    <Route path="/" element={<Main />} />
-    <Route path="/menu-food" element={<MenuOfFoof />} />
-    <Route path="/basket" element={<Basket />} />
-    <Route path="/food-detail-supplement" element={<FoodDetailSupplement />} />
-    <Route path="/food-detail" element={<FoodDetail />} />
-  </Routes>
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/menu-food" element={<MenuOfFoof />} />
+      <Route path="/basket" element={<Basket />} />
+      <Route path="/food-detail-supplement" element={<FoodDetailSupplement />} />
+      <Route path="/food-detail" element={<FoodDetail />} />
+    </Routes>
+  </>
 );
 
 export default Pages;
---
+++ src/widgets/header/ui/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  console.log(3);
+  return (
+    <div>
+      Header
+    </div>
+  );
+};
+
+export default Header;
