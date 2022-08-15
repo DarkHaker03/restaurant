0c7d2106672bde095ec7f6571e4b5c0cdec3d100
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-08-15 14:52:34 +0500

    refactor: refactor ui of shared/container

--- src/shared/container/ui/index.tsx
+++ src/shared/container/ui/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Container: FC<Props> = ({ children }) => (
-  <div style={{ margin: '16px 16px 90px 16px' }}>
+  <div style={{ margin: '16px 16px 120px 16px' }}>
     {children}
   </div>
 );
